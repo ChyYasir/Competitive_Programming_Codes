8cf710e36a82f3ea7d871dc2298f924e26f65de2
ChyYasir <chyyasir2000@gmail.com>
2021-07-07 16:38:52 +0600

    added pbds.cpp

---
+++ STL/pbds.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type, less<int> , rb_tree_tag, tree_order_statistics_node_update>
+
+
+int main(){
+    ordered_set st;
+
+    st.insert(1);
+    st.insert(3);
+    st.insert(5);
+    st.insert(5);
+    st.insert(7);
+    st.insert(8);
+
+    for(auto x: st){
+        cout << x << ' ';
+    }
+    cout << endl;
+
+    cout << st.order_of_key(6) << endl; // st.order_of_key(x) returns the number of elements less than x ;
+
+    auto it = st.find_by_order(3); // returns an iterator pointing to the 3rd index element [0-based indexing]
+
+    cout << *it << endl;
+
+    return 0;
+}
